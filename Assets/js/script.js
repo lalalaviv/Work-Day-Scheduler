5fdd43459b513b81998e9c9322c9598e49e4b0af
lalalaviv <lalala.viv@hotmail.com>
2022-09-11 18:33:11 +1000

    updated javascript file so time is added to timeblocks in HTML

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,45 +1,60 @@
+// Displays today's date in the given format
 var currentDay = moment().format("dddd, MMM Do YYYY")
 $("#currentDay").text (currentDay);
 
+// starts the day at 9am
+var startTime = moment().startOf("day").add(9, "h")
 
-var currentTime = moment().hour()
+//Adds time to HTML timeblocks
+var time9 = startTime.add(0, "h")
+$(".9am").text(time9.format("h A")); 
+
+var time10 = startTime.add(1, "h")
+$(".10am").text(time10.format("h A")); 
+
+var time11 = startTime.add(1, "h")
+$(".11am").text(time11.format("h A")); 
+
+var time12 = startTime.add(1, "h")
+$(".12pm").text(time12.format("h A")); 
+
+var time13 = startTime.add(1, "h")
+$(".1pm").text(time13.format("h A")); 
+
+var time14 = startTime.add(1, "h")
+$(".2pm").text(time14.format("h A")); 
+
+var time15 = startTime.add(1, "h")
+$(".3pm").text(time15.format("h A")); 
+
+var time16 = startTime.add(1, "h")
+$(".4pm").text(time16.format("h A")); 
+
+var time17 = startTime.add(1, "h")
+$(".5pm").text(time17.format("h A")); 
 
 
-$(function () {
-    if currentTime.isSame($('.hour'))
-    $(".past").switchClass("past", "present", 1000)
-    $("present").switchClass("present", "past", 1000);
+
+// sets the time in hour blocks, disccarding minutes; 
+var currentTime = moment().startOf("hour"); 
 
 
-})
-/**
-  * $.dateTimeHighlightNow()
-  * Author: Sam Deering
-  * Adds/removes a selected class on elements based on the current date and time.
-  * usage: $('.program p').dateTimeHighlightNow();
-  */
- jQuery.fn.dateTimeHighlightNow = function()
- {
-     return this.each(function()
-     {
-         var timestamp = Math.round(new Date().getTime() / 1000)
-             elem = $(this),
-             start = elem.attr('start'),
-             finish= elem.attr('finish');
- 
-             log('datetimestamp = '+timestamp);
- 
-         if (start  timestamp)
-         {
-             elem.addClass('selected');
-             log(elem);
-         }
-         else
-         {
-             elem.removeClass('selected');
-         }
- 
-     });
- };
+// var currentTime = moment().hour()
 
- $('.program p').dateTimeHighlight();
+// Function to highlight past present and future time by comparing current time to each timeslot
+// $(function () {
+//     if currentTime.isSame($('.hour'))
+//     $(".past").switchClass("past", "present", 1000)
+//     $("present").switchClass("present", "past", 1000);
+
+
+
+$(".saveBtn").on("click", function(event){
+event.preventDefault();
+    var eventInput = $(".form-control").val();
+    var hourList = $(".row").data("hour"); 
+    localStorage.setItem(hourList, eventInput);
+
+
+}
+)
